86ed69d311431c2e09a8da6d1b0ef65b4f492e4e
RatItBe <re228424@gmail.com>
2025-10-10 16:05:49 +0900

    feat: 1,2,3번째 페이지 명확한 분리 및 버튼으로 넘어갈 수 있도록 통합 (#9)
    Co-authored-by: RatItBe <re228424@google.com>

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 import 'normalize.css';
 import '@noonnu/bmjua';
+
+const createArea = document.querySelector(('.timer-create-area'));
+const settingModal = document.querySelector(('.timer-setting-modal'));
+const activeArea = document.querySelector(('.timer-active-area'));
+
+const createButton = document.querySelector('#create-timer');
+const confirmButton = document.querySelector('.generate-row');
+const closeButton = document.querySelector('button[aria-label="닫기"]');
+
+createButton.addEventListener('click', () => {
+  settingModal.hidden = false;
+})
+
+confirmButton.addEventListener('click', () => {
+  createArea.hidden = true;
+  settingModal.hidden = true;
+  activeArea.hidden = false;
+})
+
+closeButton.addEventListener('click', () => {
+  settingModal.hidden = true;
+})
