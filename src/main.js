f55ad729258788425a6cc3be9511aad95b1fea97
Mu hun <iam@muhun.kim>
2025-10-12 16:27:54 +0900

    chore: 프로젝트에 Prettier 포메터 실행하기
    no changes made to HTML, JavaScript, or CSS files

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import 'normalize.css';
 import '@noonnu/bmjua';
 
-const createArea = document.querySelector(('.timer-create-area'));
-const settingModal = document.querySelector(('.timer-setting-modal'));
-const activeArea = document.querySelector(('.timer-active-area'));
+const createArea = document.querySelector('.timer-create-area');
+const settingModal = document.querySelector('.timer-setting-modal');
+const activeArea = document.querySelector('.timer-active-area');
 
 const createButton = document.querySelector('#create-timer');
 const confirmButton = document.querySelector('.generate-row');
@@ -11,14 +11,14 @@ const closeButton = document.querySelector('button[aria-label="닫기"]');
 
 createButton.addEventListener('click', () => {
   settingModal.hidden = false;
-})
+});
 
 confirmButton.addEventListener('click', () => {
   createArea.hidden = true;
   settingModal.hidden = true;
   activeArea.hidden = false;
-})
+});
 
 closeButton.addEventListener('click', () => {
   settingModal.hidden = true;
-})
+});
