dc4217c2579b598c8dae8aa6b3d1e00f08381748
RatItBe <re228424@gmail.com>
2025-11-10 15:06:21 +0900

    refactor: tab 리스트를 each문으로 묶음

--- src/components/page3.svelte
+++ src/components/page3.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
+  const tabs = [
+    { keyword: 'work', label: '일할 시간', selected: true },
+    { keyword: 'break', label: '짧은 휴식', selected: false },
+    { keyword: 'long-break', label: '긴 휴식', selected: false },
+  ];
 </script>
 
 <section class="timer-active-area" hidden>
   <div class="inner-box">
     <menu class="tab-list" role="tablist">
-      <li role="tab" aria-selected="true" data-keyword="work">
-        <button>일할 시간</button>
-      </li>
-      <li role="tab" data-keyword="break">
-        <button>짧은 휴식</button>
-      </li>
-      <li role="tab" data-keyword="long-break">
-        <button>긴 휴식</button>
-      </li>
+      {#each tabs as tab}
+        <li role="tab" aria-selected={tab.selected} data-keyword={tab.keyword}>
+          <button>{tab.label}</button>
+        </li>
+      {/each}
     </menu>
     <div class="frame">
       <div class="timer-display">25:00</div>
