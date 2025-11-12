3a93a9bcc59bc1cccd850061a87d081db7be3325
RatItBe <re228424@gmail.com>
2025-11-13 03:24:58 +0900

    remove: page3 내에서 구현하여 클래스에서 미사용 중인 메소드 삭제

--- src/lib/timer.ts
+++ src/lib/timer.ts
@@ -1,5 +1,3 @@
-import { formatTime } from './utils/formatUtil';
-
 export default class Timer {
   changeState(state: TimerInfo) {
     // 이 밑은 전부 UI 부
@@ -12,13 +10,5 @@ export default class Timer {
   initTimer() {
     // todo notify 알림하는 걸 설정에서 스스로 켜게 하기? 타이머 버튼 옆에서 켜게 하기? => 왜 이걸 생각하냐면 notify 하는 것 자체는 타이머가 가질 역할이 아니고 홀로 독립되어 동작할 수 있기 때문임. (다른 버튼으로 역할 위임)
     // requestNotificationPermission();;
-  }
-
-  startTimer() {
-    // 함수 안에 있는 이 아래 부분들은 전부 ui 로 관리되는 부분
-    // 3페이지 타이머 UI 의 시작 버튼을 누를 경우 현재 timerDisplay 값으로 아래의 연산을 한 후 timer#start()의 인수에다가 넣어주면 됨.
-    const timerString = this.timerDisplay.textContent;
-    const duration = formatTime(timerString);
-    this.settingGuide.dataset.cycleContext = `${this.currentCycle}/${this.totalCycle}`;
   }
 }
